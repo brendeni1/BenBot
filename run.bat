b6113b06495fa4fdec12220d61dd5a1ba8bb9795
Brenden <brendeniannetta@gmail.com>
2025-02-10 18:17:12 -0500

    run script

--- run.bat
+++
@@ -1,3 +0,0 @@
-.venv\Scripts\activate
-
-pythn main.py
---
+++ run.ps1
@@ -0,0 +1,5 @@
+C:\Users\brend\Coding\Python\BenBot\.venv\Scripts\activate.ps1
+
+python C:\Users\brend\Coding\Python\BenBot\main.py %*
+
+pause
