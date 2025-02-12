633b35d2c130d33da25b48d707f7d66516b05db9
Brenden <brendeniannetta@gmail.com>
2025-02-12 02:15:22 -0500

    custom rich prescence

--- src/bot.py
+++ src/bot.py
@@ -2,4 +2,6 @@ import discord
 
 class Bot(discord.Bot):
     async def on_ready(self):
+        await self.change_presence(activity=discord.Activity(name="/commands", type=discord.ActivityType.listening))
+
         print(f"Logged in as {self.user}!")
