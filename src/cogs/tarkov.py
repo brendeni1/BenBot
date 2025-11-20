5b5e08af6f8c8317b99450aeef6d201430a277c1
Brenden Iannetta <brendeniannetta@gmail.com>
2025-11-19 19:36:11 -0500

    holy shit so many data types

--- src/cogs/tarkov.py
+++ src/cogs/tarkov.py
@@ -4,21 +4,55 @@ from discord.ext import commands
 
 from src.classes import *
 
-class Tarkov(commands.Cog):
+tarkovCommands = discord.SlashCommandGroup(
+    name="tarkov",
+    description="Commands for interacting with data from Escape from Tarkov.",
+    guild_ids=[799341195109203998],
+)
+
+
+class ItemCommands(commands.Cog):
     ISCOG = True
 
     def __init__(self, bot):
         self.bot: discord.Bot = bot
-        
-        self.description = "Commands for interacting with data from Escape from Tarkov."
-    
+
+        self.description = "Commands for looking up items in Escape from Tarkov."
+
+    itemCommands = discord.SlashCommandGroup(
+        name="items",
+        description="Commands for looking up items in Escape from Tarkov.",
+        guild_ids=[799341195109203998],
+        parent=tarkovCommands,
+    )
+
     # @discord.slash_command(description = "Template for commands.", guild_ids=[799341195109203998])
     # async def command(self, ctx: discord.ApplicationContext):
     #     pass
 
+
+class TaskCommands(commands.Cog):
+    ISCOG = True
+
+    def __init__(self, bot):
+        self.bot: discord.Bot = bot
+
+        self.description = "Commands for looking up tasks/quests in Escape from Tarkov."
+
+    taskCommands = discord.SlashCommandGroup(
+        name="quests",
+        description="Commands for looking up tasks/quests in Escape from Tarkov.",
+        guild_ids=[799341195109203998],
+        parent=tarkovCommands,
+    )
+    # @discord.slash_command(description = "Template for commands.", guild_ids=[799341195109203998])
+    # async def command(self, ctx: discord.ApplicationContext):
+    #     pass
+
+
 def setup(bot):
     currentFile = sys.modules[__name__]
-    
+
     for name in dir(currentFile):
         obj = getattr(currentFile, name)
 
---
+++ src/utils/tarkov.py
@@ -0,0 +1,85 @@
+import discord
+import requests
+import datetime
+
+from src.utils import dates
+from src.utils import text
+from src import constants
+
+from src.classes import *
+
+
+class Item:
+    pass
+
+
+class ContainedItem:
+    pass
+
+
+class Task:
+    pass
+
+
+class TaskItem:
+    pass
+
+
+class Trader:
+    pass
+
+
+class TraderLevel:
+    pass
+
+
+class TraderCashOffer:
+    pass
+
+
+class TraderReputationLevel:
+    pass
+
+
+class Vendor:
+    pass
+
+
+class ItemPrice:
+    pass
+
+
+class Barter:
+    pass
+
+
+class Craft:
+    pass
+
+
+class HideoutStation:
+    pass
+
+
+class HideoutStationBonus:
+    pass
+
+
+class HideoutStationLevel:
+    pass
+
+
+class RequirementHideoutStationLevel:
+    pass
+
+
+class RequirementItem:
+    pass
+
+
+class RequirementSkill:
+    pass
+
+
+class RequirementTrader:
+    pass
