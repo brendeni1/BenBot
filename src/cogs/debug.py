394e28b9585846d09f2c4e15d78d269dcdba28ba
Brenden Iannetta <brendeniannetta@gmail.com>
2025-12-10 17:41:11 -0500

    new domain woot

--- src/cogs/debug.py
+++ src/cogs/debug.py
@@ -8,28 +8,34 @@ from src.utils import dates
 
 from src.classes import *
 
+
 class Debug(commands.Cog):
     ISCOG = True
 
     def __init__(self, bot):
         self.bot = bot
-        
+
         self.description = "Contains a few debug and bot vital commands."
         self.timeStarted = time.time()
-    
-    @discord.slash_command(description = "Returns a list of vitals such as latency and uptime.", guild_ids=[799341195109203998])
+
+    @discord.slash_command(
+        description="Returns a list of vitals such as latency and uptime.",
+        guild_ids=[799341195109203998],
+    )
     async def ping(self, ctx):
         uptime = dates.formatSeconds(round(time.time() - self.timeStarted))
         latency = round(self.bot.latency * 1000)
         cpuUsage = psutil.cpu_percent(interval=1)
 
-        formatted = f"<:sus:816524395605786624>  Pong! Latency: {latency}ms. Uptime: {uptime}. CPU Usage: {cpuUsage}%.\n\nFor a list of commands and a short description, use '/commands'.\n\nSource code: https://github.com/brendeni1/BenBot"
+        formatted = f"<:sus:816524395605786624>  Pong! Latency: {latency}ms. Uptime: {uptime}. CPU Usage: {cpuUsage}%.\n\nFor a list of commands and a short description, use '/commands'.\n\nSource code: https://bb.brendenian.net"
 
         reply = EmbedReply("Ping", "debug", description=formatted)
 
         await reply.send(ctx)
-    
-    @discord.slash_command(description = "Returns a list of commands.", guild_ids=[799341195109203998])
+
+    @discord.slash_command(
+        description="Returns a list of commands.", guild_ids=[799341195109203998]
+    )
     async def commands(self, ctx):
         cogs = self.bot.cogs
 
@@ -51,9 +57,10 @@ class Debug(commands.Cog):
 
         await reply.send(ctx)
 
+
 def setup(bot):
     currentFile = sys.modules[__name__]
-    
+
     for name in dir(currentFile):
         obj = getattr(currentFile, name)
 
